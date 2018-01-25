666481ff786d6284a3abed5cb6af5a79e0ac8734
skar404 <skar404@gmail.com>
2018-01-25 15:09:31 +0300

    update ci and setup

--- setup.py
+++ setup.py
@@ -17,8 +17,6 @@ setup(name='pptx-template-fork',
       classifiers=[
           "Development Status :: 3 - Alpha",
           "Topic :: Utilities",
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
