af2e26b22d3761fdfd644b04d59cf8bc05c439be
skar404 <skar404@gmail.com>
2018-01-25 15:11:39 +0300

    ..

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(name='pptx-template-fork',
           "Programming Language :: Python :: 3",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
           "License :: OSI Approved :: Apache Software License",
           "Operating System :: OS Independent"
       ])
